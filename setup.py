c6de7c16655c846d2b79c050ee12d372d16be5c3
Christiaan Maks <christilut@gmail.com>
2014-04-17 20:37:33 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
 	name = 'TorrentMediaSearcher',
 	packages = ['torrentmediasearcher', 'torrentmediasearcher.providers'],
-	version = '1.0.1',
+	version = '1.0.2',
 	description = 'Fetches TV and movie magnet links from various torrent providers',
 	author = 'Christiaan Maks',
 	author_email = 'christiaanmaks@mylittlesky.net',
